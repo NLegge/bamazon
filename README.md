eb934f1c4c09cd7bdc830234e0b19d25a4b69c5e
Your NameNLegge <little.legge@gmail.com>
2017-10-11 17:41:10 -0700

    testing readMe markup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# bamazon
+#Bamazon: Node.js & MySQL
+
+##Overview
+Execule command line functions as one of three users:
+	*The customer can purchase items from Bamazon.
+	*The manager can View Products for Sale, View Low Inventory, Add to Inventory, or Add New Product.
+	*The supervisor can View Product Sales by Department or Create New Department.
+
+##User Guide
+
+###Customer
--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
+-- Create bamazon database
 CREATE DATABASE bamazon;
-
+-- Select bamazon database for action
 USE bamazon;
-
+-- Create products table
 CREATE TABLE products (
 item_id INT (10) AUTO_INCREMENT NOT NULL,
 product_name VARCHAR (100) NOT NULL,
@@ -10,23 +11,14 @@ price DECIMAL (10, 2) NOT NULL,
 stock_quantity INT (10) NOT NULL,
 PRIMARY KEY (item_id)
 );
-
-
+-- Add product_sales to products table for Challenge #3
 ALTER TABLE products
 ADD product_sales INT (10) NOT NULL 
 AFTER stock_quantity; 
-
+-- Create department table
 CREATE TABLE departments (
 department_id INT (10) AUTO_INCREMENT NOT NULL,
 department_name VARCHAR (100) NOT NULL,
 over_head_costs INT (10) NOT NULL,
 PRIMARY KEY (department_id)
 );
-
-
-
-SELECT departments.department_id, departments.department_name, departments.over_head_costs, 
-products.product_sales - departments.over_head_costs AS total_profit
-FROM departments 
-INNER JOIN products ON products.product_sales
-AND products.product_sales - departments.over_head_costs AS total_profit;
